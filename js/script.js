dc572c3bc8fa45c4842116bb39937bd677b6ecfc
JuliaIvakh <85705848+JuliaIvakh@users.noreply.github.com>
2021-10-08 22:25:38 +0300

    hw7

---
+++ js/script.js
@@ -0,0 +1,61 @@
+let a;
+let b;
+let result;
+a = +prompt("Write any number:");
+(result = a === 0 ? "Right" : "Wrong"), alert(result);
+// (result = a > 0 ? "Right" : "Wrong"), alert(result);
+// (result = a < 0 ? "Right" : "Wrong"), alert(result);
+// (result = a >= 0 ? "Right" : "Wrong"), alert(result);
+// (result = a <= 0 ? "Right" : "Wrong"), alert(result);
+// (result = a != 0 ? "Right" : "Wrong"), alert(result);
+// (result = (a = "test") ? "Right" : "Wrong"), alert(result);
+// (result = a === "1" ? "Right" : "Wrong"), alert(result);
+// (result = 0 < a < 5 ? "Right" : "Wrong"), alert(result);
+
+//Task 10
+// switch (a) {
+//   case 0 || 2:
+//     a += 7;
+//     break;
+//   default:
+//     a /= 10;
+//     break;
+// }
+// alert(a);
+
+// if (a === 0 || a === 2) {
+//   a += 7;
+// } else {
+//   a /= 10;
+// }
+// alert(a);
+
+// b = +prompt("Write a second number");
+
+//Task 11
+// if (a <= 1 && b >= 3) {
+//   alert(a + b);
+// } else {
+//   alert(a - b);
+// }
+
+//Task 12
+// (result = 2 < a < 11 || 6 <= b < 14 ? "Right" : "Wrong"), alert(result);
+
+//Task 13
+// let num;
+// num = +prompt("Write any number from 1 to 4");
+// switch (num) {
+//   case 1:
+//     result = alert("winter");
+//     break;
+//   case 2:
+//     result = alert("spring");
+//     break;
+//   case 3:
+//     result = alert("summer");
+//     break;
+//   case 4:
+//     result = alert("autumn");
+//     break;
+// }
